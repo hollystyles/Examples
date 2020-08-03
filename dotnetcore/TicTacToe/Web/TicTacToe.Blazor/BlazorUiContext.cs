c7afb72d5df05cfe01ad2f4863e055c526fa44fe
hollystyles <styles.holly@googlemail.com>
2020-08-03 22:48:51 +0100

    * Add blazorwasm web application as UI for TicTacToe.Lib

---
+++ dotnetcore/TicTacToe/Web/TicTacToe.Blazor/BlazorUiContext.cs
@@ -0,0 +1,55 @@
+using Hollyathome.Games.TicTacToe.Lib;
+
+namespace TicTacToe.Blazor
+{
+    public class BlazorUiContext : UiContext
+    {
+        public Player currentPlayer;
+
+        public Grid currentGrid;
+
+        public string prompt;
+
+        public string error;
+
+        public bool gameOver;
+
+        private int nextMove;
+
+        public override void Draw(Grid grid)
+        { 
+            currentGrid = grid;
+        }
+
+        public override void TakeTurn(Grid grid, Player player)
+        { 
+            currentPlayer = player;
+            currentGrid = grid;
+
+            prompt = $"Player '{player.Symbol()}' choose a free cell (1-9): ";
+        }
+
+        public void AcceptMove(int move)
+        {
+            nextMove = move;
+            currentPlayer.AttemptMove(currentGrid, move);
+        }
+
+        public override void Error(string message)
+        {
+            error = message;
+        }
+
+        public override void DeclareWinner(Player winner)
+        {
+            prompt = $"Player '{winner.Symbol()}' is the winner.";
+            gameOver = true;
+        }
+
+        public override void DeclareDrawnGame()
+        {
+            prompt = $"The game is a draw.";
+            gameOver = true;
+        }
+    }
+}
